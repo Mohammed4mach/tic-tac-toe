528cd2c32d13fc4934862e073007ea033927a455
Mohamed Abdullsalam <70759290+Mohammed4mach@users.noreply.github.com>
2022-05-03 18:38:02 +0200

    patch 0.1

--- tic_board.c
+++ tic_board.c
@@ -65,36 +65,36 @@ void turn(Tic_Matrix * pntr_tm, char entry, int x, int y)
     }
 
     // Check if entry is different from entries across column x
-    if(pntr_tm -> x[x].counter && is_x != pntr_tm -> x[x].is_x || pntr_tm -> x[x].counter == -1)
-        pntr_tm -> x[x].counter = -1;
+    if(pntr_tm -> x[x - 1].counter && is_x != pntr_tm -> x[x - 1].is_x || pntr_tm -> x[x - 1].counter == -1)
+        pntr_tm -> x[x - 1].counter = -1;
     else
     {
-        pntr_tm -> x[x].counter++;
-        pntr_tm -> x[x].is_x = is_x;
+        pntr_tm -> x[x - 1].counter++;
+        pntr_tm -> x[x - 1].is_x = is_x;
     }
     // Check if entry is different from entries across row y
-    if(pntr_tm -> y[y].counter && is_x != pntr_tm -> y[y].is_x || pntr_tm -> y[y].counter == -1)
-        pntr_tm -> y[y].counter = -1;
+    if(pntr_tm -> y[y - 1].counter && is_x != pntr_tm -> y[y - 1].is_x || pntr_tm -> y[y - 1].counter == -1)
+        pntr_tm -> y[y - 1].counter = -1;
     else
     {
-        pntr_tm -> y[y].counter++;
-        pntr_tm -> y[y].is_x = is_x;
+        pntr_tm -> y[y - 1].counter++;
+        pntr_tm -> y[y - 1].is_x = is_x;
     }
 
     // Check a win
-    if(pntr_tm -> x[x].counter == TIC_N)
-        win(pntr_tm -> x[x].is_x);
+    if(pntr_tm -> x[x - 1].counter == TIC_N)
+        win(pntr_tm -> x[x - 1].is_x);
 
-    if(pntr_tm -> y[y].counter == TIC_N)
-        win(pntr_tm -> y[y].is_x);
+    if(pntr_tm -> y[y - 1].counter == TIC_N)
+        win(pntr_tm -> y[y - 1].is_x);
 }
 
 void clear(Tic_Matrix * pntr_tm)
 {
     for(int i = 0; i < TIC_N; i++)
     {
-        pntr_tm -> x[i].counter = 0;
-        pntr_tm -> y[i].counter = 0;
+        pntr_tm -> x[i - 1].counter = 0;
+        pntr_tm -> y[i - 1].counter = 0;
     }
 
     pntr_tm -> diag1.counter = 0;
